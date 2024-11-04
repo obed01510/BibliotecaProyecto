f657669ff2305ac70655fe7e76c0576a3dac5ee0
JavierAmaya <photojavhn@gmail.com>
2024-11-04 16:53:29 -0600

    [ADD] method to test connection

--- BibliotecaProyecto/BibliotecaProyecto/frmEstudiante.cs
+++ BibliotecaProyecto/BibliotecaProyecto/frmEstudiante.cs
@@ -19,10 +19,17 @@ namespace BibliotecaProyecto
 
         private void usuariosBindingNavigatorSaveItem_Click(object sender, EventArgs e)
         {
-            this.Validate();
-            this.usuariosBindingSource.EndEdit();
-            this.tableAdapterManager.UpdateAll(this.bibliotecaProyectoDataSet);
-
+            try
+            {
+                this.Validate();
+                this.usuariosBindingSource.EndEdit();
+                this.tableAdapterManager.UpdateAll(this.bibliotecaProyectoDataSet);
+                MessageBox.Show("Datos guardados exitosamente.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al guardar los datos: {ex.Message}");
+            }
         }
 
         private void frmEstudiante_Load(object sender, EventArgs e)
@@ -30,13 +37,30 @@ namespace BibliotecaProyecto
             // TODO: esta línea de código carga datos en la tabla 'bibliotecaProyectoDataSet.Usuarios' Puede moverla o quitarla según sea necesario.
             this.usuariosTableAdapter.Fill(this.bibliotecaProyectoDataSet.Usuarios);
 
+            // Llama al método de prueba de conexión al cargar el formulario
+            TestConnection();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            frmUsuarios frmusu = new frmUsuarios(carreraTextBox.Text, anioCursoTextBox.Text); 
-
+            frmUsuarios frmusu = new frmUsuarios(carreraTextBox.Text, anioCursoTextBox.Text);
             this.Hide();
+        }
+
+        private void TestConnection()
+        {
+            try
+            {
+                using (var connection = new System.Data.OleDb.OleDbConnection(Properties.Settings.Default.BibliotecaProyectoConnectionString))
+                {
+                    connection.Open();
+                    MessageBox.Show("Conexión exitosa a la base de datos.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error de conexión: {ex.Message}");
+            }
         }
     }
 }
